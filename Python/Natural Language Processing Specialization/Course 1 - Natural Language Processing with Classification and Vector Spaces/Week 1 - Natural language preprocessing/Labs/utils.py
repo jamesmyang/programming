5b2eb6c8c4b244b7179e878a5e1ebb503fd09925
jamesmyang <jamesmyang@hotmail.com>
2025-01-27 10:27:36 -0500

    Add C1W1L1 word frequence

--- Python/Natural Language Processing Specialization/Course 1 - Natural Language Processing with Classification and Vector Spaces/Week 1 - Natural language preprocessing/Labs/utils.py
+++ Python/Natural Language Processing Specialization/Course 1 - Natural Language Processing with Classification and Vector Spaces/Week 1 - Natural language preprocessing/Labs/utils.py
@@ -1,6 +1,8 @@
 import re                                  # library for regular expression operations
 import string                              # for string operations
 
+import numpy as np 
+
 from nltk.corpus import stopwords          # module for stop words that come with NLTK
 from nltk.stem import PorterStemmer        # module for stemming
 from nltk.tokenize import TweetTokenizer   # module for tokenizing strings
@@ -39,3 +41,30 @@ def process_tweet(tweet):
             
 
     return tweets_clean
+
+def build_freqs(tweets, ys):
+    """Build frequencies.
+    Input:
+        tweets: a list of tweets
+        ys: an m x 1 array with the sentiment label of each tweet
+            (either 0 or 1)
+    Output:
+        freqs: a dictionary mapping each (word, sentiment) pair to its
+        frequency
+    """
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
+    yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
+    freqs = {}
+    for y, tweet in zip(yslist, tweets):
+        for word in process_tweet(tweet):
+            pair = (word, y)
+            if pair in freqs:
+                freqs[pair] += 1
+            else:
+                freqs[pair] = 1    
+    return freqs
